c5576b6885b63bf1ea169356e96c2ad4b4e94c6f
unknown <lucie.le.rolland@gmail.com>
2017-07-08 18:02:03 +0800

    putain

--- scripts/extractInput.py
+++ scripts/extractInput.py
@@ -1,73 +1,75 @@
 import pandas as pd
 
-url = '/home/lucie/Bureau/2015-2016 LSE/Side work/RA/'
+data_path = '../data/'
 
-def fullInput(url):
 
-	input = pd.read_csv(url+ 'input/input'+str(0)+'.csv')
+def full_input(dir_prefix):
 
-	i=1
-	while i < 148:
-		read = pd.DataFrame({})
-		read = pd.read_csv(url+ 'input/input'+str(i)+'.csv')
-		input = input.append(read, ignore_index=True)
-		i=i+1
+    input_csv =pd.read_csv(dir_prefix + 'input/input'+str(0)+'.csv')
 
-	return input
+    i = 1
+    while i < 148:
+        read = pd.read_csv(dir_prefix + 'input/input'+str(i)+'.csv')
+        input_csv = input_csv.append(read, ignore_index=True)
+        i = i+1
 
-def bioInput(url):
-	input = pd.read_csv(url+ 'input/input'+str(0)+'.csv')
-	input = input[input.iplace == 1]
+    return input_csv
 
-	i=1
-	while i < 148:
-		read = pd.DataFrame({})
-		read = pd.read_csv(url+ 'input/input'+str(i)+'.csv')
-		read = read[read.ibio == 1]
-		input = input.append(read, ignore_index=True)
-		i=i+1
 
-	return input
+def bio_input(dir_prefix):
+    input_csv = pd.read_csv(dir_prefix + 'input/input'+str(0)+'.csv')
+    input_csv = input_csv[input_csv.iplace == 1]
 
-def bioNoOrteInput(url):
-	input = pd.read_csv(url+ 'input/input'+str(0)+'.csv')
-	input = input[(input.ibio == 1) & (input.iplace == 0)]
+    i = 1
+    while i < 148:
+        read = pd.read_csv(dir_prefix + 'input/input'+str(i)+'.csv')
+        read = read[read.ibio == 1]
+        input_csv = input_csv.append(read, ignore_index=True)
+        i = i+1
 
-	i=1
-	while i < 148:
-		read = pd.DataFrame({})
-		read = pd.read_csv(url+ 'input/input'+str(i)+'.csv')
-		read = read[(read.ibio == 1) & (read.iplace == 0)]
-		input = input.append(read, ignore_index=True)
-		i=i+1
+    return input_csv
 
-	return input
 
-def bioOrteInput(url):
-	input = pd.read_csv(url+ 'input/input'+str(0)+'.csv')
-	input = input[(input.ibio == 1) & (input.iplace == 1)]
+def bio_no_orte_input(dir_prefix):
+    input_csv = pd.read_csv(dir_prefix + 'input/input'+str(0)+'.csv')
+    input_csv = input_csv[(input_csv.ibio == 1) & (input_csv.iplace == 0)]
 
-	i=1
-	while i < 148:
-		read = pd.DataFrame({})
-		read = pd.read_csv(url+ 'input/input'+str(i)+'.csv')
-		read = read[(read.ibio == 1) & (read.iplace == 1)]
-		input = input.append(read, ignore_index=True)
-		i=i+1
+    i = 1
+    while i < 148:
+        read = pd.read_csv(dir_prefix + 'input/input'+str(i)+'.csv')
+        read = read[(read.ibio == 1) & (read.iplace == 0)]
+        input_csv = input_csv.append(read, ignore_index=True)
+        i = i+1
 
-	return input
+    return input_csv
 
-def bioIntoPeopleDic(matrix, dic1={}):
-	dic2 = dic1
-	i=0
-	while i < len(matrix):
-		if matrix['idn'][i] != '':
-			try:
-				dic2[matrix['idn'][i]]['leben'] = matrix['leben'][i]
-			except KeyError:
-				dic2[matrix['idn'][i]] = {}
-				dic2[matrix['idn'][i]]['idn'] = matrix['idn'][i]
-				dic2[matrix['idn'][i]]['name'] = matrix['name'][i]
-				dic2[matrix['idn'][i]]['leben'] = matrix['leben'][i]
-		i=i+1
-	return dic2
+
+def bio_orte_input(dir_prefix):
+    input_csv = pd.read_csv(dir_prefix + 'input/input'+str(0)+'.csv')
+    input_csv = input_csv[(input_csv.ibio == 1) & (input_csv.iplace == 1)]
+
+    i = 1
+    while i < 148:
+        read = pd.read_csv(dir_prefix + 'input/input'+str(i)+'.csv')
+        read = read[(read.ibio == 1) & (read.iplace == 1)]
+        input_csv = input_csv.append(read, ignore_index=True)
+        i = i+1
+
+    return input_csv
+
+
+def bio_into_people_dic(matrix, dic1):
+
+    dic2 = dic1
+    i = 0
+    while i < len(matrix):
+        if matrix['idn'][i] != '':
+            try:
+                dic2[matrix['idn'][i]]['leben'] = matrix['leben'][i]
+            except KeyError:
+                dic2[matrix['idn'][i]] = {}
+                dic2[matrix['idn'][i]]['idn'] = matrix['idn'][i]
+                dic2[matrix['idn'][i]]['name'] = matrix['name'][i]
+                dic2[matrix['idn'][i]]['leben'] = matrix['leben'][i]
+        i = i+1
+    return dic2
--- scripts/extractOutput.py
+++ scripts/extractOutput.py
@@ -1,115 +1,117 @@
 import pandas as pd
 
-url = '../data/'
+data_path = '../data/'
 
-def fullOutput(url):
 
-	output = pd.read_csv(url+ 'output/output'+str(0)+'.csv')
+def full_output(dir_prefix):
 
-	i=1
-	while i < 148:
-		read = pd.DataFrame({})
-		read = pd.read_csv(url+ 'output/output'+str(i)+'.csv')
-		output = output.append(read, ignore_index=True)
-		i=i+1
+    output = pd.read_csv(dir_prefix + 'output/output'+str(0)+'.csv')
 
-	return output
+    i = 1
+    while i < 148:
+        read = pd.read_csv(dir_prefix + 'output/output'+str(i)+'.csv')
+        output = output.append(read, ignore_index=True)
+        i = i+1
 
-def orteOutput(url):
-	output = pd.read_csv(url+ 'output/output'+str(0)+'.csv')
-	output = output[output.iplace == 1]
+    return output
 
-	i=1
-	while i < 148:
-		read = pd.DataFrame({})
-		read = pd.read_csv(url+ 'output/output'+str(i)+'.csv')
-		read = read[read.iplace == 1]
-		output = output.append(read, ignore_index=True)
-		i=i+1
 
-	return output
+def orte_output(dir_prefix):
+    output = pd.read_csv(dir_prefix + 'output/output'+str(0)+'.csv')
+    output = output[output.iplace == 1]
 
-def orteNoBioOutput(url):
-	output = pd.read_csv(url+ 'output/output'+str(0)+'.csv')
-	output = output[(output.iplace == 1) & (output.ibio == 0)]
+    i = 1
+    while i < 148:
+        read = pd.read_csv(dir_prefix + 'output/output'+str(i)+'.csv')
+        read = read[read.iplace == 1]
+        output = output.append(read, ignore_index=True)
+        i = i+1
 
-	i=1
-	while i < 148:
-		read = pd.DataFrame({})
-		read = pd.read_csv(url+ 'output/output'+str(i)+'.csv')
-		read = read[(read.iplace == 1) & (read.ibio == 0)]
-		output = output.append(read, ignore_index=True)
-		i=i+1
+    return output
 
-	return output
 
-def orteBioOutput(url):
-	output = pd.read_csv(url+ 'output/output'+str(0)+'.csv')
-	output = output[(output.iplace == 1) & (output.ibio == 1)]
+def orte_no_bio_output(dir_prefix):
+    output = pd.read_csv(dir_prefix + 'output/output'+str(0)+'.csv')
+    output = output[(output.iplace == 1) & (output.ibio == 0)]
 
-	i=1
-	while i < 148:
-		read = pd.DataFrame({})
-		read = pd.read_csv(url+ 'output/output'+str(i)+'.csv')
-		read = read[(read.iplace == 1) & (read.ibio == 1)]
-		output = output.append(read, ignore_index=True)
-		i=i+1
+    i = 1
+    while i < 148:
+        read = pd.read_csv(dir_prefix + 'output/output'+str(i)+'.csv')
+        read = read[(read.iplace == 1) & (read.ibio == 0)]
+        output = output.append(read, ignore_index=True)
+        i = i+1
 
-	return output
+    return output
 
-def orteIntoPeopleDic(matrix, dic1={}):
-	dic2 = dic1
-	i=0
-	while i < len(matrix):
-		if matrix['idn'][i] != '':
-			try:
-				if 'orte' in dic2[matrix['idn'][i]].keys():
-					dic2[matrix['idn'][i]]['orte'][matrix['funct'][i]] = matrix['cities'][i]
-				else:
-					dic2[matrix['idn'][i]]['orte'] = {}
-					dic2[matrix['idn'][i]]['orte'][matrix['funct'][i]] = matrix['cities'][i]
-			except KeyError:
-				dic2[matrix['idn'][i]] = {}
-				dic2[matrix['idn'][i]]['idn'] = matrix['idn'][i]
-				dic2[matrix['idn'][i]]['name'] = matrix['name'][i]
-				if 'orte' in dic2[matrix['idn'][i]].keys():
-					dic2[matrix['idn'][i]]['orte'][matrix['funct'][i]] = matrix['cities'][i]
-				else:
-					dic2[matrix['idn'][i]]['orte'] = {}
-					dic2[matrix['idn'][i]]['orte'][matrix['funct'][i]] = matrix['cities'][i]
-		i=i+1
 
-	return dic2
+def orte_bio_output(dir_prefix):
+    output = pd.read_csv(dir_prefix + 'output/output'+str(0)+'.csv')
+    output = output[(output.iplace == 1) & (output.ibio == 1)]
 
-def locationList(matrix):
-	loc = {}
-	i=0
-	while i < len(matrix):
-		if matrix['cities'][i] != '' and matrix['cities'][i] not in loc.keys():
-			loc[matrix['cities'][i]] = 1
-		else:
-			loc[matrix['cities'][i]] = loc[matrix['cities'][i]] + 1
-		i=i+1
+    i = 1
+    while i < 148:
+        read = pd.read_csv(dir_prefix + 'output/output'+str(i)+'.csv')
+        read = read[(read.iplace == 1) & (read.ibio == 1)]
+        output = output.append(read, ignore_index=True)
+        i = i+1
 
-	return loc
+    return output
 
-#print(len(locationList(orteBioOutput(url))))
 
-d=locationList(orteOutput(url))
-dlist = []
+def orte_into_people_dic(matrix, dic1):
+    dic2 = dic1
+    i = 0
+    while i < len(matrix):
+        if matrix['idn'][i] != '':
+            try:
+                if 'orte' in dic2[matrix['idn'][i]].keys():
+                    dic2[matrix['idn'][i]]['orte'][matrix['funct'][i]] = matrix['cities'][i]
+                else:
+                    dic2[matrix['idn'][i]]['orte'] = {}
+                    dic2[matrix['idn'][i]]['orte'][matrix['funct'][i]] = matrix['cities'][i]
+            except KeyError:
+                dic2[matrix['idn'][i]] = {}
+                dic2[matrix['idn'][i]]['idn'] = matrix['idn'][i]
+                dic2[matrix['idn'][i]]['name'] = matrix['name'][i]
+                if 'orte' in dic2[matrix['idn'][i]].keys():
+                    dic2[matrix['idn'][i]]['orte'][matrix['funct'][i]] = matrix['cities'][i]
+                else:
+                    dic2[matrix['idn'][i]]['orte'] = {}
+                    dic2[matrix['idn'][i]]['orte'][matrix['funct'][i]] = matrix['cities'][i]
+        i = i+1
+
+    return dic2
+
+
+def location_list(matrix):
+    loc = {}
+    i = 0
+    while i < len(matrix):
+        if matrix['cities'][i] != '' and matrix['cities'][i] not in loc.keys():
+            loc[matrix['cities'][i]] = 1
+        else:
+            loc[matrix['cities'][i]] = loc[matrix['cities'][i]] + 1
+        i = i+1
+
+    return loc
+
+# print(len(location_list(orte_bio_output(data_path))))
+
+d=location_list(orte_output(data_path))
+d_list = []
 
 for key, value in d.items():
-    temp = [key,value]
-    dlist.append(temp)
+    temp = [key, value]
+    d_list.append(temp)
 
-dlist.sort(key=lambda x:-x[1])
+d_list.sort(key=lambda x: -x[1])
 
-for i in range(0,99):
-	print(dlist[i])
+for k in range(0, 99):
+    print(d_list[k])
 
-#print(len(locationList(orteNoBioOutput(url))))
+# print(len(location_list(orte_no_bio_output(data_path))))
 
-#print(len(locationList(orteOutput(url))))
+# print(len(locationList(orte_output(data_path))))
 
 
 
