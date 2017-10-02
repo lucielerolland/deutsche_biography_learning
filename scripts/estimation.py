Added tests, fixed gradient value

scripts/estimation.py
@@ -39,8 +39,8 @@ def build_x_and_y(path):
     city_list = []
     sentence = []
 
-    for k in people:
-    # for k in ['136810942', '139526781', '129102687', '138361193', '116119160', '119108445', '118925563']:
+    # for k in people:
+    for k in ['136810942', '139526781', '129102687', '138361193', '116119160', '119108445', '118925563']:
         for c0 in set(ref_cities_clean):
             is_a_city_match, add_sentence = cbm.city_match_sentence(full_dic[k]['leben'], c0)
             if is_a_city_match == 1:
@@ -131,55 +131,52 @@ iterations_list = [1000]
 # iterations_list = [100, 300, 1000, 3000, 10000, 30000, 100000]
 
 # alpha_list = [0.01]
-# alpha_list = [0.00001, 0.00003, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300, 1000]
+alpha_list = [0.00001, 0.00003, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300, 1000]
 
-alpha_list = [1, 3, 10, 30, 100, 300, 1000]
-
-l_list = [0.]
+l_list = [0.001]
 # l_list = [0.00001, 0.00003, 0.0001, 0.0003, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300, 1000]
 
 # beta = np.mat(np.random.randn(x.shape[1], y_dummies.shape[1]))*0.01
-
-beta = np.mat(np.zeros((x.shape[1], y_dummies.shape[1])))
 
 cost = []
 index = []
 
 print("Starting training at time : " + str(datetime.now()))
 
-print(lr.gradient_checker(x=x, y=y, beta=beta, epsilon=epsilon, l=l_list[0], has_constant=True))
-#for l in l_list:
-#    for alpha in alpha_list:
-#        for iterations in iterations_list:
-#            for i in range(iterations):
-#                beta = lr.gradient_descent(alpha=alpha, x=train_x, beta=beta, y=train_y_dummies, l=l, activation='softmax', has_constant=True)
-#                cost.append(lr.cost(train_x, beta, train_y_dummies, l, 'softmax'))
+# print(lr.gradient_checker(x=train_x, y=train_y_dummies, beta=beta, epsilon=epsilon, l=l_list[0], has_constant=True))
+for l in l_list:
+    for alpha in alpha_list:
+        for iterations in iterations_list:
+            beta = np.mat(np.random.randn(x.shape[1], y_dummies.shape[1]))
+            for i in range(iterations):
+                beta = lr.gradient_descent(alpha=alpha, x=train_x, beta=beta, y=train_y_dummies, l=l, activation='softmax', has_constant=True)
+                cost.append(lr.cost(train_x, beta, train_y_dummies, l, 'softmax'))
 
-#                if i % 100 == 0:
-#                    print(lr.cost(train_x, beta, train_y_dummies, l, 'softmax'))
+                if i % 100 == 0:
+                    print(lr.cost(train_x, beta, train_y_dummies, l, 'softmax'))
 #                    print(lr.gradient_checker(x=x, y=y, beta=beta, epsilon=epsilon, l=l_list[0], has_constant=True))
 
             # Compute perf on test
 
-#            pred_y_train = lr.pred(train_x, beta)
-#            pred_y_test = lr.pred(test_x, beta)
+            pred_y_train = lr.pred(train_x, beta)
+            pred_y_test = lr.pred(test_x, beta)
 
-#            exact_pred_train = 0
-#            exact_pred_test = 0
+            exact_pred_train = 0
+            exact_pred_test = 0
 
-#            total_train = 0
-#            total_test = 0
+            total_train = 0
+            total_test = 0
 
-#            for i in range(np.shape(pred_y_train)[0]):
-#                total_train += 1
-#                if pred_y_train[i] == train_y[i]:
-#                    exact_pred_train += 1
+            for i in range(np.shape(pred_y_train)[0]):
+                total_train += 1
+                if pred_y_train[i] == train_y[i]:
+                    exact_pred_train += 1
 
-#            for i in range(np.shape(pred_y_test)[0]):
-#                total_test += 1
-#                if pred_y_test[i] == test_y[i]:
-#                    exact_pred_test += 1
+            for i in range(np.shape(pred_y_test)[0]):
+                total_test += 1
+                if pred_y_test[i] == test_y[i]:
+                    exact_pred_test += 1
 
-#            print('At time : ' + str(datetime.now()) + ', l : ' + str(l) + ', alpha : ' + str(alpha) +
-#                  ', iter : ' + str(iterations) + ', score train : ' + str(exact_pred_train/total_train) +
-#                  ', score test : ' + str(exact_pred_test/total_test))
+            print('At time : ' + str(datetime.now()) + ', l : ' + str(l) + ', alpha : ' + str(alpha) +
+                  ', iter : ' + str(iterations) + ', score train : ' + str(exact_pred_train/total_train) +
+                  ', score test : ' + str(exact_pred_test/total_test))

scripts/log_regression.py
@@ -10,7 +10,7 @@ def softmax(matrix):
     exp_matrix = np.exp(matrix)
     try:
         sum_exp_matrix = np.sum(exp_matrix, axis=1, keepdims=True)
-    except ValueError:
+    except :
         sum_exp_matrix = np.sum(exp_matrix, axis=1)
 
     return exp_matrix/sum_exp_matrix
@@ -25,31 +25,34 @@ def y_to_dummies(vector, K):
 
 
 def cost(x, beta, y, l, activation, has_constant=True):
+
     m = np.shape(x)[0]
     if activation == 'sigmoid':
         y_hat = sigmoid(x.dot(beta))
-        direct_cost_vect = -np.multiply(y, y_hat) - np.multiply((1-y), (1-y_hat))
-        reg = (l/(2*m))*np.transpose(beta).dot(beta)
+        direct_cost_vect = -np.multiply(y, np.log(y_hat)) - np.multiply((1-y), np.log(1-y_hat))
+        reg = (l/(2*m))*np.square(beta)
         full_cost = 1/m*np.sum(direct_cost_vect) + reg
 
     elif activation == 'softmax':
 
         y_hat = softmax(x.dot(beta))
-        direct_cost_vect = -np.multiply(y,y_hat)
+        direct_cost_vect = np.sum(-np.multiply(y, np.log(y_hat)))
         reg = (l / 2) * np.multiply(beta, beta)
         if has_constant:
             reg[:, 0] = 0
-        full_cost = (1 / m) * (np.sum(direct_cost_vect) + np.sum(reg))
+        reg = np.sum(reg)
+        full_cost = (1 / m) * (direct_cost_vect + reg)
 
     return full_cost
 
 
 def gradient(x, beta, y, l, activation, has_constant=True):
     m = np.shape(x)[0]
+    k = np.shape(x)[1]
+    class_num = np.shape(y)[1]
     if has_constant:
-        k = np.shape(x)[1]
         mult = np.identity(k, dtype=float)
-        mult[0,0] = 0
+        mult[0, 0] = 0
         reg_gradient = (l/m)*mult.dot(beta)
     else:
         reg_gradient = (l/m)*beta
@@ -57,7 +60,7 @@ def gradient(x, beta, y, l, activation, has_constant=True):
     if activation == 'sigmoid':
         direct_gradient = (1 / m) * np.transpose(x).dot(sigmoid(x.dot(beta)) - y)
     elif activation == 'softmax':
-        direct_gradient = (1 / m) * np.transpose(x).dot(softmax(x.dot(beta)) - y)
+        direct_gradient = (1 / (2*m)) * np.transpose(x).dot(softmax(x.dot(beta)) - y)
     grad = direct_gradient + np.mat(reg_gradient)
 
     return grad
@@ -77,7 +80,7 @@ def pred(x, beta):
 
 
 def grad_mat_to_vector(matrix):
-    vector = matrix.reshape(matrix.shape[0]*matrix.shape[1],1)
+    vector = matrix.reshape(matrix.shape[0]*matrix.shape[1], 1)
 
     return vector
 
@@ -85,25 +88,30 @@ def grad_mat_to_vector(matrix):
 def grad_vec_to_mat(vector, shape):
     matrix = vector.reshape(shape)
 
-
     return matrix
 
 
 def gradient_checker(x, y, beta, epsilon, l, has_constant=True):
+
     beta_shape = (beta.shape[0], beta.shape[1])
+#    beta_name = np.zeros(beta_shape)
+    #    for i in beta_shape[0]:
+    #    for j in beta_shape[1]:
+    #        beta_name[i, j] = 'beta[' + str(i) + ', ' + str(j) + ']'
     true_grad = grad_mat_to_vector(gradient(x, beta, y, l, 'softmax', has_constant))
     vec_beta = grad_mat_to_vector(beta)
+#    vec_beta_name = grad_mat_to_vector(beta_name)
     Jplus = np.zeros((len(vec_beta), 1))
     Jminus = np.zeros((len(vec_beta), 1))
     fake_grad = np.zeros((len(vec_beta), 1))
     for i in range(len(vec_beta)):
         thetaplus = vec_beta
         thetaplus[i] = thetaplus[i] + epsilon
-        Jplus[i] = cost(x, grad_vec_to_mat(thetaplus, beta_shape), y, l, 'softmax')
+        Jplus[i] = cost(x, grad_vec_to_mat(thetaplus, beta.shape), y, l, 'softmax')
 
         thetaminus = vec_beta
         thetaminus[i] = thetaminus[i] - epsilon
-        Jminus[i] = cost(x, grad_vec_to_mat(thetaminus, beta_shape), y, l, 'softmax')
+        Jminus[i] = cost(x, grad_vec_to_mat(thetaminus, beta.shape), y, l, 'softmax')
 
         fake_grad[i] = (Jplus[i] - Jminus[i])/(2*epsilon)
 

scripts/test_log_regression.py
@@ -4,28 +4,65 @@ import numpy as np
 
 
 @pytest.yield_fixture()
-def ones(dimensions):              # dimensions should be a 2-d tuple
-    ret_ones = np.ones(dimensions)
+def ones(dim_one):              # dimensions should be a 2-d tuple
+    ret_ones = np.ones(dim_one)
     yield ret_ones
 
 
 @pytest.yield_fixture()
-def zeros(dimensions):
-    ret_zeros = np.zeros(dimensions)
+def zeros(dim_zero):
+    ret_zeros = np.zeros(dim_zero)
     yield ret_zeros
 
 
 @pytest.yield_fixture()
-def random(dimensions):
-    ret_random = np.random.randn(dimensions[0], dimensions[1])
+def random(dim_random):
+    ret_random = np.random.randn(dim_random[0], dim_random[1])
     yield ret_random
 
 
 @pytest.yield_fixture()
-def random_one_infinity(dimensions):
-    ret_random_one_infinity = np.random.randn(dimensions[0], dimensions[1])
+def random_one_infinity(dim_random_infty):
+    ret_random_one_infinity = np.random.randn(dim_random_infty[0], dim_random_infty[1])
     ret_random_one_infinity[0,0] = 40
     yield ret_random_one_infinity
+
+
+@pytest.yield_fixture()
+def t_test(size_t, ones_vec):   # y should be a matrix, ones should be a list of positions for ones encoded as tuples
+    t = np.zeros(size_t)
+    for k in ones_vec:
+        t[k] = 1
+    return t
+
+
+@pytest.yield_fixture()
+def beta_test_random(size_beta):   # beta should be a matrix
+    beta_test = np.random.randn(size_beta[0], size_beta[1])
+    return beta_test
+
+
+@pytest.yield_fixture()
+def beta_test_zero(size_beta):   # beta should be a matrix
+    beta_test = np.zeros(size_beta)
+    return beta_test
+
+
+@pytest.yield_fixture()
+def beta_test_identity(size_beta):   # beta should be a matrix
+    beta_test = np.eye(size_beta[0], size_beta[1])
+    return beta_test
+
+
+@pytest.yield_fixture()
+def x_test_random(size_x):   # x should be a matrix
+    x_test = np.random.randn(size_x[0], size_x[1])
+    return x_test
+
+
+@pytest.yield_fixture()
+def l():
+    return np.random.randn(1, 1)[0,0]
 
 
 def test_sigmoid_scalar():
@@ -34,7 +71,8 @@ def test_sigmoid_scalar():
     assert round(lr.sigmoid(10)) == 1
 
 
-@pytest.mark.parametrize('dimensions', [(10, 1)])
+@pytest.mark.parametrize('dim_one', [(10, 1)])
+@pytest.mark.parametrize('dim_zero', [(10, 1)])
 def test_sigmoid_vector(ones, zeros):
     for i in range(10):
         assert lr.sigmoid(zeros)[i] == ones[i]*1/2
@@ -42,7 +80,10 @@ def test_sigmoid_vector(ones, zeros):
         assert round(lr.sigmoid(ones*10)[i, 0] - ones[i, 0], 3) == 0
 
 
-@pytest.mark.parametrize('dimensions', [(10, 1)])
+@pytest.mark.parametrize('dim_one', [(10, 1)])
+@pytest.mark.parametrize('dim_zero', [(10, 1)])
+@pytest.mark.parametrize('dim_random', [(10, 1)])
+@pytest.mark.parametrize('dim_random_infty', [(10, 1)])
 def test_softmax_vector(ones, zeros, random, random_one_infinity):
     random_t = random.T
     assert lr.softmax(random).shape[0] == random.shape[0]
@@ -53,19 +94,85 @@ def test_softmax_vector(ones, zeros, random, random_one_infinity):
     for i in range(10):
         assert round(lr.softmax(zeros)[i, 0] - ones[i, 0], 3) == 0
         assert lr.softmax(zeros.T)[0, i] == ones.T[0, i]*1/10
-    if i != 0:
-        assert round(lr.softmax(random_one_infinity.T)[0, i], 3) == 0
 
 
-@pytest.mark.parametrize('dimensions', [(10, 6)])
-def test_softmax_vector(ones, zeros, random, random_one_infinity):
+@pytest.mark.parametrize('dim_one', [(10, 6)])
+@pytest.mark.parametrize('dim_zero', [(10, 6)])
+@pytest.mark.parametrize('dim_random', [(10, 6)])
+@pytest.mark.parametrize('dim_random_infty', [(10, 6)])
+def test_softmax_matrix(ones, zeros, random, random_one_infinity):
     random_t = random.T
     assert lr.softmax(random).shape[0] == random.shape[0]
     assert lr.softmax(random).shape[1] == random.shape[1]
     assert lr.softmax(random_t).shape[0] == random_t.shape[0]
     assert lr.softmax(random_t).shape[1] == random_t.shape[1]
+    assert round(lr.softmax(random_one_infinity)[0, 0]-1, 3) == 0
     assert round(lr.softmax(random_one_infinity.T)[0, 0]-1, 3) == 0
     for i in range(10):
+        assert round(np.sum(lr.softmax(random), axis=1)[i]-1, 5) == 0
         for j in range(6):
             assert lr.softmax(zeros)[i, j] == ones[i, j]*(1/6)
             assert lr.softmax(zeros.T)[j, i] == ones.T[j, i]*1/10
+    for j in range(6):
+        assert round(np.sum(lr.softmax(random_t), axis=1)[j]-1, 5) == 0
+
+
+@pytest.mark.parametrize('size_t', [(10, 5)])
+@pytest.mark.parametrize('ones_vec', [[(0, 1), (1, 4), (2, 2), (3, 1), (4, 3), (5, 1), (6, 4), (7, 2), (8, 1), (9, 3)]])
+@pytest.mark.parametrize('size_beta', [(5, 5)])
+def test_cost_softmax_equality(t_test, beta_test_identity, l):
+    assert round(lr.cost(t_test*40, beta_test_identity, t_test, 0, 'softmax', has_constant=True) + 1, 5) == 0
+    reg = (l * 5) / (2 * 10)
+    assert round(lr.cost(t_test*40, beta_test_identity, t_test, l, 'softmax', has_constant=False) + 1-reg, 5) == 0
+    reg = (l * 4) / (2 * 10)
+    assert round(lr.cost(t_test*40, beta_test_identity, t_test, l, 'softmax', has_constant=True) + 1-reg, 5) == 0
+
+
+@pytest.mark.parametrize('size_t', [(10, 5)])
+@pytest.mark.parametrize('ones_vec', [[]])
+@pytest.mark.parametrize('size_x', [(10, 7)])
+@pytest.mark.parametrize('size_beta', [(7, 5)])
+def test_cost_softmax_zeros(x_test_random, t_test, beta_test_identity, l):
+    assert round(lr.cost(x_test_random, beta_test_identity, t_test, 0, 'softmax', has_constant=True), 5) == 0
+    reg1 = (l * 5) / (2 * 10)
+    assert round(lr.cost(x_test_random, beta_test_identity, t_test, l, 'softmax', has_constant=False) - reg1, 5) == 0
+    reg2 = (l * 4) / (2 * 10)
+    assert round(lr.cost(x_test_random, beta_test_identity, t_test, l, 'softmax', has_constant=True) - reg2, 5) == 0
+
+
+@pytest.mark.parametrize('size_t', [(10, 5)])
+@pytest.mark.parametrize('ones_vec', [[(0, 1), (1, 4), (2, 2), (3, 1), (4, 3), (5, 1), (6, 4), (7, 2), (8, 1), (9, 3)]])
+@pytest.mark.parametrize('size_x', [(10, 7)])
+@pytest.mark.parametrize('size_beta', [(7, 5)])
+def test_cost_softmax_inequality(t_test, x_test_random, beta_test_random, l):
+    beta_test_first_element_pos = beta_test_random
+    beta_test_first_element_pos[0, 0] = 20
+    x_test_first_column_pos = x_test_random
+    x_test_first_column_pos[:, 0] = 20
+    assert round(lr.cost(x_test_first_column_pos, beta_test_first_element_pos, t_test, 0, 'softmax', has_constant=True), 5) == 0
+    reg1 = l / (2 * 10) * np.sum(np.square(beta_test_first_element_pos))
+    assert round(lr.cost(x_test_first_column_pos, beta_test_first_element_pos, t_test, l, 'softmax', has_constant=False) -reg1, 5) == 0
+    reg2 = l / (2 * 10) * (np.sum(np.square(beta_test_first_element_pos)) - np.sum(np.square(beta_test_first_element_pos[:, 0])))
+    assert round(lr.cost(x_test_first_column_pos, beta_test_first_element_pos, t_test, l, 'softmax', has_constant=True) -reg2, 5) == 0
+
+
+@pytest.mark.parametrize('size_t', [(10, 5)])
+@pytest.mark.parametrize('ones_vec', [[(0, 1), (1, 4), (2, 2), (3, 1), (4, 3), (5, 1), (6, 4), (7, 2), (8, 1), (9, 3)]])
+@pytest.mark.parametrize('size_beta', [(5, 5)])
+def test_gradient_softmax_equality(t_test, beta_test_identity):
+    for i in range(beta_test_identity.shape[0]):
+        for j in range(beta_test_identity.shape[1]):
+            assert round(lr.gradient(t_test*40, beta_test_identity, t_test, 0, 'softmax', has_constant=True)[i,j], 5) == 0
+
+
+@pytest.mark.parametrize('dim_random', [(10, 1)])
+def test_grad_mat_to_vector_vector(random):
+    for i in range(10):
+        assert lr.grad_mat_to_vector(random)[i] == random[i]
+
+
+@pytest.mark.parametrize('dim_random', [(10, 7)])
+def test_grad_mat_to_vector_vector(random):
+    for i in range(10):
+        for j in range(7):
+            assert lr.grad_mat_to_vector(random)[j+i*7] == random[i, j]